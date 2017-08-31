0466931005c3045c61159ef8610508ced3920033
rick22 Alcazar <rickity_rick_22@hotmail.com>
2017-08-31 12:37:44 -0700

    second push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,33 @@
-$("#addUser").on("click", Function(event) {
-  event.preventDefault();
-}
+(function() {
 
 
-var userName, emailAddress, password;
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAHL1gUv1RYc-YWV1zK72UDW29r1lVV1K4",
+    authDomain: "foupon-6c9dd.firebaseapp.com",
+    databaseURL: "https://foupon-6c9dd.firebaseio.com",
+    projectId: "foupon-6c9dd",
+    storageBucket: "foupon-6c9dd.appspot.com",
+    messagingSenderId: "774072237770"
+  };
+  firebase.initializeApp(config);
 
-// TODO: read these values from a form where the user entered them
+  // Get ELements
+  const txt Email = document.getElementById("txtEmail");
+  const txt Password = document.getElementById("txtPassword");
+  const btnLogin = document.getElementById("btnLogin");
+  const btnSignUp = document.getElementById("btnSignUp");
+  const btnLogout = document.getElementById("btnLogout");
 
-var ref = new Firebase("https://www.gstatic.com/firebasejs/4.3.0/firebase.js");
-ref.createUser({
-  email    : emailAddress,
-  password : password
-}, function(error, authData) {
-  if (error) {
-    console.log("Error creating user:", error);
-  } else {
-    // save the user's profile into the database so we can list users,
-    // use them in Security and Firebase Rules, and show profiles
-    ref.child("users").child(authData.uid).set({
-      provider: authData.provider,
-      name: userName
-    });
-  }
-});cd 
+  // add login event
+  btnLogin.addEventListener("click", e => {
+    // get email an pass
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    //  sign in
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+  })
+
+
