6f8213ad8883d4cb468123881ffaa06440d4b717
annacrespo12 <annacrespo12@gmail.com>
2017-09-01 13:25:43 -0700

    extra body tag and added js src

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,15 @@
-function initialize {
-	
-}
+// $(document).ready(function() {
+//     addStartingButtons();
+// });
+// $("button").click(function() {
+
+//         var fired_button = $(this).val().trim();
+//         event.preventDefault();
+//         let queryURL = "https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJSx6SrQ9T2YARed8V_f0hOg0&type=restaurant" + "location" + "&key=AIzaSyDcvNrflCgCWKKMnOXp4q8gcDNAftiSPew"
+//         $.ajax({ url: queryURL, method: 'GET' })
+//             .done(function(response) {
+                
+//                     $('#gifArea').prepend("<img  src='" + response.data[i].images.fixed_height_still.url + "' >");
+//                     $('#gifArea').prepend("<p>Rating: " + response.data[i].rating + "</p>");
+//                     console.log(response);
+
