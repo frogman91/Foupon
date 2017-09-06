c2a3de7008cc0a6eaecc31e086a1f7ee1cd4cb26
rick22 Alcazar <rickity_rick_22@hotmail.com>
2017-09-06 11:55:28 -0700

    firebase likes updated

--- assets/javascript/firebase_app.js
+++ assets/javascript/firebase_app.js
@@ -4,22 +4,34 @@
     // Initial Values
     
     var email = "";
-    
+    var likes = "";
+
+    var userObject = {};
+
+
     // Capture Button Click
     $("#add-user").on("click", function(event) {
       event.preventDefault();
       
-      // Grabbed values from text-boxes
+      // Grabbed values from html
         email = $("#inputEmail").val().trim();
-                  
-      // Code for "Setting values in the database"
-      database.ref().set({
-        
-        email: email,
-         
-      });
-
     });
+        
+    // $("#likeButton").on("click", function(event) {
+    //   event.preventDefault();
+
+    //   // Grabbed like values
+    //     likes = $("#newHeader").val().trim() 
+                  
+    //   // Code for "Setting values in the database"
+    //   database.ref().set({
+        
+    //     email: email,
+    //     likes: likes,
+         
+    //   });
+
+    // });
 
     // Firebase watcher + initial loader 
     database.ref().on("value", function(snapshot) {
@@ -28,6 +40,7 @@
       console.log(snapshot.val());
       
       console.log(snapshot.val().email);
+      console.log(snapshot.val().likes);
       
       
 
@@ -41,3 +54,77 @@
     }, function(errorObject) {
       console.log("Errors handled: " + errorObject.code);
     });
+
+
+  var btnLogout = document.getElementById("btnLogout");
+
+  
+  // when user signs up
+  $("#add-user").on("click", function(e){
+    e.preventDefault();
+    var email = $("#inputEmail").val().trim();
+    var pass = $("#inputPassword").val().trim();
+    var auth = firebase.auth();
+    //  sign in
+    var promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+  });
+
+    // when user signs up
+  $("#login-user").on("click", function(e){
+    e.preventDefault();
+    var email = $("#loginEmail").val().trim();
+    var pass = $("#loginPassword").val().trim();
+    var auth = firebase.auth();
+    //  sign in
+    firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+      // Handle Errors here.
+      console.log(error);
+      // ...
+    });
+  });
+
+  $("#logout-user").on("click", function(e){
+    e.preventDefault();
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+      alert("user was logged out");
+    }).catch(function(error) {
+      // An error happened.
+      console.log(error);
+    });
+  })
+
+
+  //check to see if the user is signed in
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      userObject.email = user.email;
+      userObject.uid = user.uid;
+
+      // checking in the userLikes database 
+      database.ref("/userLikes").on("child_added", function(snapshot){
+        // if the email from the database matches the email from the 
+        // logged in user
+        if(snapshot.val().userEmail === userObject.email) {
+          // show the user likes
+          console.log('our user likes', snapshot.val().liked);
+        }
+      })
+    } else {
+      // No user is signed in.
+    }
+  });
+
+  //when the user click the like button 
+  $("#likeButton").on("click", function(e){
+    e.preventDefault();
+    database.ref("/userLikes").push({
+      userEmail: userObject.email, 
+      userId: userObject.uid,
+      liked: $("#search-input").val().trim()
+    }); 
+  })
+  // it saves the like to the database 
+  // and is tied back the user 
